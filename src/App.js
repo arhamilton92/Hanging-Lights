d375b098b88b4f1e31061c8fed77ce1189f4c98a
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-18 16:20:50 -0400

    created visual icons for character, item, pet, and gallery pages.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 import icon from './images/Flora.png'
 import garwyvern from './images/batsy.png'
+import peticon from './images/pomicon.png'
+import galleryicon from './images/gallery.png'
+import itemicon from './images/items.png'
+import charactericon from './images/characters.png'
 
 function App() {
   return (
     <div className="App">
-      <div class="container-fluid page-header text-center pt-4">
-        <div class="row">
-          <div class="col parent">
-            <h1 class="title center">HANGING LIGHTS</h1>
-            <p></p>
-          </div>
-        </div>
-      </div>
+      <Header />
       <div className="container mt-3">
         <div class="row">
           <div class="col nav-button">
@@ -51,20 +49,29 @@ function App() {
         </div>
         <div className="row pb-5" id="display">
           <div className="col-12">
-            <div className="row mb-4 pt-3 pb-3" id="display-nav">
-              <div className="col-12">
-                <div className="row">
-                  <div className="col mb-2 mt-2 parent">
-                    <h5 className="profile-button">CHARACTERS</h5>
+            <div className="row mb-5" id="display-nav">
+              <div className="col-12 parent">
+                <div className="row center">
+                  <div className="col-sm-6 col-lg-2 mb-2 mt-2 parent nav-element">
+                    <img src={charactericon} class="nav-icon"></img>
                   </div>
-                  <div className="col mb-2 mt-2 parent">
-                    <h5 className="profile-button">INVENTORY</h5>
+                  <div className="d-none d-lg-block dot">
+                    <h1></h1>
                   </div>
-                  <div className="col mb-2 mt-2 parent">
-                    <h5 className="profile-button">GARDENS</h5>
+                  <div className="col-sm-6 col-lg-2  mb-2 mt-2 parent nav-element">
+                    <img src={peticon} class="nav-icon"></img>
                   </div>
-                  <div className="col mb-2 mt-2 parent">
-                    <h5 className="profile-button">GALLERY</h5>
+                  <div className="d-none d-lg-block dot">
+                    <h1></h1>
+                  </div>
+                  <div className="col-sm-6 col-lg-2  mb-2 mt-2 parent nav-element">
+                    <img src={itemicon} class="nav-icon"></img>
+                  </div>
+                  <div className="d-none d-lg-block dot">
+                    <h1></h1>
+                  </div>
+                  <div className="col-sm-6 col-lg-2  xs-2 mt-2 parent nav-element">
+                    <img src={galleryicon} class="nav-icon"></img>
                   </div>
                 </div>
               </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div class="container-fluid page-header text-center pt-4">
+                <div class="row">
+                <div class="col parent">
+                    <h1 class="title center">HANGING LIGHTS</h1>
+                    <p></p>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
